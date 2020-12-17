89c9d03e36497725df75c15cbc7eb339fc15ae7d
vi-k <victor.dunaev@gmail.com>
2020-12-17 18:20:27 +1000

    Вдогонку

--- places/lib/ui/res/text_styles.dart
+++ places/lib/ui/res/text_styles.dart
@@ -6,16 +6,10 @@ final textRegular = _text.copyWith(fontWeight: FontWeight.normal);
 final textMiddle = textRegular.copyWith(fontWeight: FontWeight.w500);
 final textBold = textRegular.copyWith(fontWeight: FontWeight.bold);
 
-// final textBoldWhite = textBold.copyWith(color: Colors.white);
+final textRegular16 = textRegular.copyWith(fontSize: 16, height: 1.25);
+final textMiddle16 = textMiddle.copyWith(fontSize: 16, height: 1.25);
+final textBold16 = textBold.copyWith(fontSize: 16, height: 1.25);
 
-// final textRegular16 = textRegular.copyWith(fontSize: 16, height: 1.25);
-// final textMiddle16 = textMiddle.copyWith(fontSize: 16, height: 1.25);
-// final textBold16 = textBold.copyWith(fontSize: 16, height: 1.25);
-
-// final textBold18 = textBold.copyWith(fontSize: 18, height: 1.25);
-
-// final textRegular24 = textRegular.copyWith(fontSize: 24, height: 1.2);
-// final textMiddle24 = textMiddle.copyWith(fontSize: 24, height: 1.2);
-// final textBold24 = textBold.copyWith(fontSize: 24, height: 1.2);
-
-// final textBold32 = textBold.copyWith(fontSize: 32, height: 1.125);
+final textBold18 = textBold.copyWith(fontSize: 18, height: 1.25);
+final textBold24 = textBold.copyWith(fontSize: 24, height: 1.2);
+final textBold32 = textBold.copyWith(fontSize: 32, height: 1.125);
