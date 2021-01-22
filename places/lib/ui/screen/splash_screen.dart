6801bcd296a7776eacd8cecd376337cd6480f901
vi-k <victor.dunaev@gmail.com>
2021-01-22 14:27:54 +1000

    Добавил SplashScreen

---
+++ places/lib/ui/screen/splash_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'onboarding_screen.dart';
+
+/// Сплэш-скрин.
+/// 
+/// Ждёт завершения инициализации. Показывается минимум 2 сек.
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  /// Флаг инициализации приложения. Ставим временную заглушку в 1 сек.
+  final isInitialized =
+      Future<bool>.delayed(const Duration(seconds: 1), () => true);
+
+  @override
+  void initState() {
+    super.initState();
+
+    _navigateToNext();
+  }
+
+  // ignore: avoid_void_async
+  void _navigateToNext() async {
+    final sw = Stopwatch();
+
+    // Ждём завершения инициализации приложения. Но держим SplashScreen минимум
+    // 2 сек.
+    print('Инициализация приложения');
+    sw.start();
+    await Future.wait([
+      isInitialized,
+      Future<bool>.delayed(const Duration(seconds: 2), () => true),
+    ]);
+    sw.stop();
+    print('Затрачено: ${sw.elapsed}');
+    print('Переход на следующий экран');
+
+    await Navigator.push<int>(
+      context,
+      MaterialPageRoute(
+        builder: (context) => OnboardingScreen(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) => const Scaffold(
+        body: Center(
+          child: Text('Splash Screen'),
+        ),
+      );
+}
